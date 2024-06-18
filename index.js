07abd1939f90e63c2507a2d09796d88b4addf3ab
rramKatwal <rramkatwal24@gmail.com>
2024-06-18 20:49:33 +0545

    adding mern authentication project

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import Database from "./functions/database.js";
+import dotenv from "dotenv";
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
+
+const port = process.env.PORT || 8080;
+
+app.use(express.static(path.resolve(__dirname, "client", "dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+});
+
+app.listen(port, () => {
+  Database();
+});
